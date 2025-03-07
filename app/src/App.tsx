ef30a017feb80b1ea8e422f81980d8012284bb5a
Brian Dillmann <dillmann.brian@gmail.com>
2025-03-06 23:25:29 -0500

    create react app boilerplate

---
+++ app/src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import ThingComponent from "./components/ThingComponent";
+import OtherComponent from "./components/Other/OtherComponent";
+
+const App: React.FC = () => {
+  return <div></div>;
+};
+
+export default App;

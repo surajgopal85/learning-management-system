07ae4c5c5e40c41017a1ffd00e57e8089e6c0b6d
Brian Dillmann <dillmann.brian@gmail.com>
2025-03-06 23:26:06 -0500

    first commit, front end

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -4,7 +4,27 @@ import ThingComponent from "./components/ThingComponent";
 import OtherComponent from "./components/Other/OtherComponent";
 
 const App: React.FC = () => {
-  return <div></div>;
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/other">Other</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<ThingComponent />} />
+          <Route path="/other" element={<OtherComponent />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 };
 
 export default App;
